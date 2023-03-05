6bd6c76196e2ef15e50c4b3f32c61bbe8c4a9d2a
Will914 <57054984+Miastonished@users.noreply.github.com>
2023-03-05 20:06:04 +1100

    v1.0
    version 1.0 of open FTS

--- FTS.java
+++ FTS.java
@@ -9,6 +9,12 @@ import javax.imageio.*;
 import javax.swing.*;
 import java.util.Random;
 
+// Open Find The Silvers v1.0
+//  Created by Miastonished
+// https://github.com/Miastonished/openFTS
+
+// Silver created by silver
+// https://silvershorthair.github.io/
 
 public class FTS extends JFrame implements ActionListener{
 
@@ -36,6 +42,7 @@ public class FTS extends JFrame implements ActionListener{
     static int r = rand.nextInt(592 * 2);
     static int r2 = rand.nextInt(251 * 2);
     
+    static int score;
     
     public static void main(String[] args) {
         
@@ -54,36 +61,44 @@ public class FTS extends JFrame implements ActionListener{
         frame.setSize(691 * scale, 350 * scale);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        // creates the window
+        //creates the window
+        
         
         hitbox = new JButton("");         
         hitbox.setBounds(r+(15 * scale),r2, 70 * scale,98 * scale);
-        frame.add(hitbox);
         hitbox.setContentAreaFilled(false);
         hitbox.setBorderPainted(false);
         hitbox.addActionListener(new FTS());
-        //makes the hitbox for silver
+        //creates the hitbox for silver
+        
         
         eggsImg = new JLabel(new ImageIcon(new ImageIcon(egg).getImage().getScaledInstance(250 * scale, 150 * scale, Image.SCALE_DEFAULT)));         
         eggsImg.setBounds(50 * scale, 150 * scale, 250 * scale, 150 * scale);
-        frame.add(eggsImg); //     creates the eggs
+        //creates the eggs
         
         
         silverImg = new JLabel(new ImageIcon(new ImageIcon(sil).getImage().getScaledInstance(100 * scale, 100 * scale, Image.SCALE_DEFAULT)));         
         silverImg.setBounds(r,r2,100 * scale,100 * scale);
-        frame.add(silverImg); //   creates silver
+        //creates silver
         
         
         basketImg = new JLabel(new ImageIcon(new ImageIcon(bas).getImage().getScaledInstance(250 * scale, 150 * scale, Image.SCALE_DEFAULT)));         
         basketImg.setBounds(400 * scale, 150 * scale, 250 * scale, 150 * scale);
-        frame.add(basketImg); //   creates the basket of eggs
+        //creates the basket of eggs
         
         
         bgImg = new JLabel(new ImageIcon(new ImageIcon(bg).getImage().getScaledInstance(691 * scale, 350 * scale, Image.SCALE_DEFAULT)));
         bgImg.setBounds(0 * scale, 0 * scale, 691 * scale, 350 * scale);
-        frame.add(bgImg); //       creates the background
+        //creates the background
         
+        
+        frame.add(hitbox);
+        frame.add(eggsImg);
+        frame.add(silverImg);
+        frame.add(basketImg);
+        frame.add(bgImg);
         frame.revalidate();
+        //shoves all the stuff into the frame
     }
 
     public void actionPerformed(ActionEvent e) {
@@ -97,6 +112,10 @@ public class FTS extends JFrame implements ActionListener{
             silverImg.setBounds(r,r2,100 * scale,100 * scale);
             frame.revalidate();
             // ^ moves silver and silver's hitbox
+            
+            score++;
+            System.out.println(score);
+            frame.setTitle("openFindTheSilvers | "+score+" Silvers Found");
         }
     }
     
