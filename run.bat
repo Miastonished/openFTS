a557a34d9a7e44935c8b0b7c0806ad81121ff6df
Will914 <57054984+Miastonished@users.noreply.github.com>
2023-04-09 18:42:36 +1000

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-javac FTS.java -Xdiags:verbose -Xlint:deprecation -Xlint:unchecked
-java FTS
+@echo off & set a="0"
+javac FTS.java -Xdiags:verbose -Xlint:deprecation -Xlint:unchecked && java FTS && set a="1"
+if %a%=="0" set /p id=""
